3fe634de4cd9103dadad4f101e365a8da9835be7
Faniry-lang <faniryranaivoson167@gmail.com>
2025-06-27 08:38:30 +0300

    added comments on test.ts

--- back-core/src/test.ts
+++ back-core/src/test.ts
@@ -5,7 +5,8 @@ import { AdminService } from './admin/admin.service';
 async function bootstrap() {
   // pour tester les fonctions des services en lignes de commandes
   // NB: ne pas utiliser les controllers ici
-  
+  // pour compiler et executer: npx ts-node [le nom du fichier].ts
+
   const app = await NestFactory.createApplicationContext(AppModule);
 
   const adminService = app.get(AdminService);
