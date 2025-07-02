0385890990c1fd02cd4c9a60aeba5597fe6f793f
Faniry-lang <faniryranaivoson167@gmail.com>
2025-07-02 20:23:20 +0300

    update on project structure

--- back-core/src/chef-cuisinier/chef-cuisinier.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChefCuisinierController } from './chef-cuisinier.controller';
-import { ChefCuisinierService } from './chef-cuisinier.service';
-
-@Module({
-  controllers: [ChefCuisinierController],
-  providers: [ChefCuisinierService]
-})
-export class ChefCuisinierModule {}
